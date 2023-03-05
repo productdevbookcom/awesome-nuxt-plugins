a0e16abdb064084c3eee6c1572c81825fa0e72ed
productdevbook <hi@productdevbook.com>
2023-03-05 11:49:01 +0300

    feat(v3): supertokens

---
+++ v3/supertokens.client.ts
@@ -0,0 +1,28 @@
+import SuperTokens from 'supertokens-web-js/index.js'
+import Session from 'supertokens-web-js/recipe/session/index.js'
+import ThirdPartyEmailPassword from 'supertokens-web-js/recipe/thirdpartyemailpassword/index.js'
+import EmailVerification from 'supertokens-web-js/recipe/emailverification'
+
+export default defineNuxtPlugin((_nuxtApp) => {
+  const env = useRuntimeConfig()
+  const app = SuperTokens.init({
+    appInfo: {
+      apiDomain: env.public.api,
+      apiBasePath: '/auth',
+      appName: 'sudeb',
+    },
+    recipeList: [
+      EmailVerification.init(),
+      Session.init({
+        tokenTransferMethod: env.public.mobile ? 'header' : 'cookie',
+      }),
+      ThirdPartyEmailPassword.init(),
+    ],
+  })
+
+  return {
+    provide: {
+      authApp: app,
+    },
+  }
+})
